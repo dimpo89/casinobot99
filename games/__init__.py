d067215e50542c49af81d5de7d98b615147e5ae3
dimpo89 <dgeh08296@gmail.com>
2026-03-12 14:48:44 +0300

    Create __init__.py

---
+++ games/__init__.py
@@ -0,0 +1,22 @@
+from .base_game import BaseGame, ProvablyFair
+from .dice import DiceGame
+from .roulette import RouletteGame
+from .mines import MinesGame
+from .plinko import PlinkoGame
+from .keno import KenoGame
+from .doghouse import DogHouseGame
+from .sugarrush import SugarRushGame
+from .blackjack import BlackjackGame
+
+__all__ = [
+    'BaseGame',
+    'ProvablyFair',
+    'DiceGame',
+    'RouletteGame',
+    'MinesGame',
+    'PlinkoGame',
+    'KenoGame',
+    'DogHouseGame',
+    'SugarRushGame',
+    'BlackjackGame'
+]
